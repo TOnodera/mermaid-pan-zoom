2ceaf1de11922cb478414415cf627860db5918ef
onodera <onodera@h-company.com>
2025-07-19 12:25:57 +0900

    react-svg-pan-zoom導入

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Mermaid from './components/Mermaid';
+import { ReactSVGPanZoom, type Value } from 'react-svg-pan-zoom';
 
 declare const acquireVsCodeApi: () => {
   postMessage: (msg: { type: string }) => void;
@@ -9,6 +10,12 @@ const vscode = acquireVsCodeApi();
 
 function App() {
   const [mermaid, setMermaid] = useState<string>('');
+  const viewerWidth = 400;
+  const viewerHeight = 400;
+  const svgWidth = 1440;
+  const svgHeight = 1440;
+  const panZoomRef = useRef<ReactSVGPanZoom | null>(null);
+  const [value, setValue] = useState<Value | null>(null);
 
   const handleMessage = (event: MessageEvent) => {
     const message = event.data;
@@ -32,6 +39,26 @@ function App() {
   return (
     <div className="flex items-center justify-center h-screen">
       <Mermaid id={uuid} mermaidText={mermaid} />
+      <ReactSVGPanZoom
+        ref={panZoomRef}
+        width={viewerWidth}
+        height={viewerHeight}
+        value={value}
+        onChangeValue={setValue}
+        tool="none"
+        modifierKeys={['Alt', 'Shift', 'Control']}
+        onChangeTool={() => { }}
+        onClick={() => { }}
+        onDoubleClick={() => { }}
+        onMouseDown={() => { }}
+        onMouseUp={() => { }}
+      >
+        <svg
+          width={svgWidth}
+          height={svgHeight}
+        >
+        </svg>
+      </ReactSVGPanZoom>
     </div>
   );
 }
--- webview-ui/src/components/Mermaid.tsx
+++ webview-ui/src/components/Mermaid.tsx
@@ -1,23 +1,60 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import mermaid from 'mermaid';
+import { ReactSVGPanZoom, type Value } from 'react-svg-pan-zoom';
+
+
+async function generateMermaidSvg(id: string, mermaidText: string): Promise<string | null> {
+    try {
+        const { svg } = await mermaid.render(id, mermaidText);
+        return svg;
+    } catch (error) {
+        console.error('Error rendering Mermaid diagram:', error);
+        return null;
+    }
+}
 
 interface Props {
-  id: string;
-  mermaidText: string;
+    id: string;
+    mermaidText: string;
 }
 //https://mermaid.js.org/config/usage.html
 export default function Mermaid({ id, mermaidText }: Props) {
-  const ref = useRef<HTMLDivElement>(null);
+    const panZoomRef = useRef<ReactSVGPanZoom>(null);
+    const svgRef = useRef<HTMLDivElement | null>(null);
+    const [value, setValue] = useState<Value | null>(null);
 
-  useEffect(() => {
-    if (ref.current) {
-      mermaid.render(id, mermaidText).then(({ svg }) => {
-        if (ref.current) {
-          ref.current.innerHTML = svg;
+    const onLoad = async (id: string, mermaidText: string) => {
+        const svg = await generateMermaidSvg(id, mermaidText);
+        if (!svg) {
+            console.error('Failed to generate Mermaid SVG');
+            return;
         }
-      });
+        if (svgRef.current) {
+            svgRef.current.innerHTML = svg;
+        }
     }
-  }, [id, mermaidText]);
 
-  return <div ref={ref} className="mermaid" />;
+    useEffect(() => {
+        onLoad(id, mermaidText);
+    }, [id, mermaidText]);
+
+    return <ReactSVGPanZoom
+        style={{ width: '100%', height: '100%' }}
+        ref={panZoomRef}
+        width={0}
+        height={0}
+        value={value}
+        onChangeValue={setValue}
+        tool="none"
+        modifierKeys={['Alt', 'Shift', 'Control']}
+        onChangeTool={() => { }}
+        onClick={() => { }}
+        onDoubleClick={() => { }}
+        onMouseDown={() => { }}
+        onMouseUp={() => { }}
+    >
+        <div
+            ref={svgRef}
+        />
+    </ReactSVGPanZoom>
 }
